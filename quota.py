f7955c988c5886569452ed29221ea097b1213a92
LiliR123 <lrossiter34@gmail.com>
2017-03-21 11:41:53 +0000

    prints quota

--- quota.py
+++ quota.py
@@ -1,20 +1,21 @@
 import sqlite3
 import csv
 
-conn=sqlite3.connect('Tutor.db')
+
+conn=sqlite3.connect('students.db')
 cursor = conn.cursor()
 
 in_id = input('Enter the tutors ID: ')
 
 while (in_id ==""):
     in_id = input('Enter the tutors ID: ')
-else: next 
+
   
 print('No of tutees', in_id ,'has is :' )
 
-cursor.execute ("SELECT students assigned FROM Tutor WHERE ID= ?",(in_id))
+cursor.execute ("SELECT StudentsAssigned FROM Tutors WHERE Tutor_ID= ?",(in_id))
 
-rows = conn.fetchall()
+rows = cursor.fetchall()
 
 for row in rows:
     print(row)
