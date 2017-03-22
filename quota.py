2d363c6dd226399f6d91111fddfea9b91ae2ca25
LiliR123 <lrossiter34@gmail.com>
2017-03-22 11:21:57 +0000

    Update quota.py
    fully working with Gui, please test for errors, thanks

--- quota.py
+++ quota.py
@@ -1,23 +1,39 @@
+from tkinter import *
+from tkinter import ttk
 import sqlite3
 import csv
 
+TITLE_FONT = ("Helvetica", 18 )
+FONT=("Helvetica", 13)
+root=Tk()
 
-conn=sqlite3.connect('students.db')
-cursor = conn.cursor()
 
-in_id = input('Enter the tutors ID: ')
+inp=StringVar()
 
-while (in_id ==""):
-    in_id = input('Enter the tutors ID: ')
+root.title("Personal Tutor System")
 
-  
-print('No of tutees', in_id ,'has is :' )
+def getvalue():
+    conn=sqlite3.connect('students.db')
+    cursor = conn.cursor()
 
-cursor.execute ("SELECT StudentsAssigned FROM Tutors WHERE Tutor_ID= ?",(in_id))
 
-rows = cursor.fetchall()
+    cursor.execute ("SELECT StudentsAssigned FROM Tutors WHERE Tutor_ID= ?",(inp.get(),))
+    
+    if( len(inp.get()) == 0):
+        next
+    else:
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            Label(root, text=" The quota of tutees Tutor " + inp.get()+" has is: " ,font=FONT).grid(row=10,column=0,sticky=W, columnspan=3)
+            Label(root, text=rows , font=FONT).grid(row=10,column=1,sticky=W, columnspan=2)
+    
+Label(root,text="Quota of the tutees",font=TITLE_FONT).grid(row=0, column=1,sticky=W)
+Label(root, text="The sytem will only return data for valid ID's so if no output is returned please re enter the ID ",font=FONT).grid(row=2, column=0, columnspan=2)
+Label(root,text="Please enter a tutor ID: ", font=FONT).grid(row=3, column=0, sticky=W)
+Entry(root, width=50, textvariable= inp).grid(row=3, column=1, sticky=W)
+Button(root, text="Enter", command=getvalue).grid(row=3, column=2, sticky=W)
 
-for row in rows:
-    print(row)
 
+root.mainloop()
 
