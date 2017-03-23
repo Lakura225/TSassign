61bc1ed39e8162483a4f2deba1338076a0ca2b10
Gitcatsquid <almorgan@live.com>
2017-03-23 11:52:20 +0000

    No longer fancy looking but it works

--- displaytutees.py
+++ displaytutees.py
@@ -11,8 +11,8 @@ class App(Frame):
         Frame.__init__(self, parent)
         self.connection = sqlite3.connect('Students.db')
         self.cur = self.connection.cursor()
-        self.CreateUI()
-        #self.DisplayThem()
+        #self.CreateUI()
+        self.DisplayThem()
         self.grid(sticky = (N,S,W,E))
         parent.grid_rowconfigure(0, weight = 1)
         parent.grid_columnconfigure(0, weight = 1)
@@ -34,20 +34,30 @@ class App(Frame):
         tv.column('Acad_Year', anchor='center', width=100)
         tv.heading('Univ_Email', text='Email')
         tv.column('Univ_Email', anchor='center', width=100)
-        self.DisplayThem()
         tv.grid(sticky = (N,S,W,E))
         self.treeview = tv
         self.grid_rowconfigure(0, weight = 1)
         self.grid_columnconfigure(0, weight = 1)
         
-    def DisplayThem(tk.frame):
+    def DisplayThem(self):
         data = self.ReadDB()
-        tk.Frame.__init__(self, parent, background="black")
+        Label(self.master, text='Student Code').grid(row=0, column=0)
+        Label(self.master, text='Surname').grid(row=0, column=1)
+        Label(self.master, text='Forename1').grid(row=0, column=2)
+        Label(self.master, text='Forename2').grid(row=0, column=3)
+        Label(self.master, text='Tutor').grid(row=0, column=4)
+        Label(self.master, text='Year').grid(row=0, column=5)
+        Label(self.master, text='Tutor Code').grid(row=0, column=6)
+        Label(self.master, text='Email').grid(row=0, column=7)
         for index, dat in enumerate(data):
-            label = tk.Label(self, text="%s/%s" % (row, column), 
-                                 borderwidth=0, width=10)
-            label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
-            current_row.append(label)
+            Label(self.master, text=dat[0]).grid(row=index+1, column=0)
+            Label(self.master, text=dat[1]).grid(row=index+1, column=1)
+            Label(self.master, text=dat[2]).grid(row=index+1, column=2)
+            Label(self.master, text=dat[3]).grid(row=index+1, column=3)
+            Label(self.master, text=dat[4]).grid(row=index+1, column=4)
+            Label(self.master, text=dat[5]).grid(row=index+1, column=5)
+            Label(self.master, text=dat[6]).grid(row=index+1, column=6)
+            Label(self.master, text=dat[7]).grid(row=index+1, column=7)
 
     def ReadDB(self):
         self.cur.execute("SELECT * FROM Student_List WHERE Tutor = ?",(Search,))
@@ -55,6 +65,7 @@ class App(Frame):
 
 def main():
     root = Tk()
+    root.title("List of Students")
     App(root)
     root.mainloop()
 
